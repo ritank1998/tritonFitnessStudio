0df2456632755aadabc0a5fd3c741e480069aab8
Ritank saxena <68276497+ritank1998@users.noreply.github.com>
2023-12-17 21:44:42 +0530

    New updates

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -3,32 +3,45 @@ import { jsx, css } from "@emotion/core";
 import React from "react";
 import Button from "../GlobalComponents/Button";
 import ContactBg from "../Image/contactBg.jpg";
+import emailjs from 'emailjs-com';
+import "../Contact/contact.css"
+
+const sendEmail = async (fullName, email, subject, message) => {
+  try {
+      const templateParams = {
+          from_name: fullName,
+          to_email: email,
+          subject: subject,
+          message: message,
+      };
+
+      const result = await emailjs.send(
+          'YOUR_SERVICE_ID',
+          'YOUR_TEMPLATE_ID',
+          templateParams,
+          'YOUR_USER_ID'
+      );
+
+      console.log('Email sent!', result);
+      return true;
+  } catch (error) {
+      console.error('Error sending email:', error);
+      return false;
+  }
+};
 
 
-function sendWhatsAppMessage() {
-  const name = document.getElementById('nameInput').value;
-  const email = document.getElementById('emailInput').value;
-  const subject = document.getElementById('subjectInput').value;
-  const message = document.getElementById('messageInput').value;
 
-  let whatsappMessage = `Name: ${name}%0AEmail: ${email}%0ASubject: ${subject ? subject : 'None'}%0AMessage: ${message ? message : 'None'}`;
 
-  const phoneNumber = '1234567890'; // Replace with your WhatsApp number
-  const encodedMessage = encodeURIComponent(whatsappMessage);
-
-  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
-
-  window.location.href = whatsappLink;
-}
 const Form = ({ text }) => (
   
 <div css={styles} className="formContainer">
   <form id="myForm">
     <input type="text" id="nameInput" placeholder="Your Name*" />
-    <input type="text" id="emailInput" placeholder="Your Email*" />
+    <input type="text" id="numberInput" placeholder="Your Email*" />
     <input type="text" id="subjectInput" placeholder="Subject" />
     <textarea id="messageInput" cols="30" rows="10" placeholder="Message"></textarea>
-    <button onclick="sendWhatsAppMessage()">SEND MESSAGE</button>
+    <button onclick="sendEmail(fullName, email, subject, message)" className="btn">SEND MESSAGE</button>
   </form>
 </div>
 
